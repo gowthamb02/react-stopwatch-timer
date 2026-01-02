6c2760354391098561e61461822c3d6201c0fd66
Gowtham B <86523805+gowthamb02@users.noreply.github.com>
2026-01-02 21:28:57 +0530

    Clean up unused imports and state in App.js
    Removed commented-out imports and state declarations.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-// import ComponentA from './component/componenA';
-// import ComponentB from './component/componentB';
-// import B from "./component/B";
-// import A from "./component/A";
 import Stopwatch from './component/stopwatch';
 
 function App() {
-  // const [msgToB, setMsgToB] = useState("");
-  // const [msgToA, setMsgToA] = useState("");
   return (
     <div>
-      {/* <ComponentA setMsgToB={setMsgToB} msgToA={msgToA} />
-      <ComponentB msgToB={msgToB} setMsgToA={setMsgToA} /> */}
-
-
       <Stopwatch />
-      {/* <A /> */}
     </div>
   );
 }
